335c68c918aaf7dea42fa19e4c2d4ed94435f891
HadeerAzzam <hdyrmdht263@gmail.com>
2023-12-16 15:01:36 +0200

    up

---
+++ src/rat_in_a_maze_problem/Rat_in_a_Maze_Problem.java
@@ -0,0 +1,23 @@
+
+package rat_in_a_maze_problem;
+
+import GUI.Main;
+
+/**
+ *
+ * @author Peter
+ */
+public class Rat_in_a_Maze_Problem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Main x=new Main();
+    x.setVisible(true);
+   
+
+    }
+
+}
